676a61ba5e054e9b89531692a814179a2a7577d0
Shekoofeh Dezhahanj <sh.dezhahanj@gmail.com>
2025-10-30 14:13:04 +0330

    task1: get data

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -12,14 +12,17 @@ public_users.post("/register", (req,res) => {
 
 // Get the book list available in the shop
 public_users.get('/',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  return res.send(JSON.stringify(books, null, 4));
 });
 
 // Get book details based on ISBN
 public_users.get('/isbn/:isbn',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  const { isbn } = req.params;
+  const book = books[isbn];
+  if (!book) {
+    return res.status(404).json({ message: "Book not found" });
+  }
+  return res.send(JSON.stringify(book, null, 4));
  });
   
 // Get book details based on author
